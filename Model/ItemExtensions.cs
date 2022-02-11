7a741a497288ef01ee73a894441f4bcef134e4d0
Yamini Punyavathi Muttevi <m.yamini91@gmail.com>
2022-02-11 16:20:10 +0530

    Get DataSource From Item
    Get DataSource From Item.

---
+++ Model/ItemExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Layouts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Foundation.SXAExtensions.Scriban.Model
+{
+    public static class ItemExtensions
+    {
+        public static RenderingReference[] GetRenderingReferences(this Item i)
+        {
+            if (i == null)
+            {
+                return new RenderingReference[0];
+            }
+            return i.Visualization.GetRenderings(Sitecore.Context.Device, false);
+        }
+
+        public static List<Item> GetDataSourceItems(this Item i)
+        {
+            List<Item> list = new List<Item>();
+            foreach (RenderingReference reference in i.GetRenderingReferences())
+            {
+                Item dataSourceItem = reference.GetDataSourceItem();
+                if (dataSourceItem != null)
+                {
+                    list.Add(dataSourceItem);
+                }
+            }
+            return list;
+        }
+
+        public static Item GetDataSourceItem(this RenderingReference reference)
+        {
+            if (reference != null)
+            {
+                return GetDataSourceItem(reference.Settings.DataSource, reference.Database);
+            }
+            return null;
+        }
+
+        private static Item GetDataSourceItem(string id, Database db)
+        {
+            Guid itemId;
+            return Guid.TryParse(id, out itemId)
+            ? db.GetItem(new ID(itemId))
+            : db.GetItem(id);
+        }
+    }
+}
---
+++ Pipelines/GenerateScribanContext/GetDataSourceFromItem.cs
@@ -0,0 +1,54 @@
+﻿using Scriban.Runtime;
+using Sitecore.Data.Items;
+using Sitecore.Mvc.Presentation;
+using Sitecore.XA.Foundation.Abstractions;
+using Sitecore.XA.Foundation.Scriban.Pipelines.GenerateScribanContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Foundation.SXAExtensions.Scriban.Model
+
+namespace Sitecore.Foundation.Scriban.Pipelines.GenerateScribanContext
+{
+    public class GetDataSourceFromItem : IGenerateScribanContextProcessor
+    {
+
+        private readonly IContext context;
+        protected readonly IPageMode PageMode;
+        private delegate List<string> Delegate(Item item);
+
+        public GetDataSourceFromItem(IPageMode pageMode, IContext context)
+        {
+            PageMode = pageMode;
+            this.context = context;
+        }
+
+        public void Process(GenerateScribanContextPipelineArgs args)
+        {
+            args.GlobalScriptObject.Import("sc_getdatasource", new Delegate(GetDataSourceFromItemImpl));
+        }
+
+        public List<string> GetDataSourceFromItemImpl(Item item)
+        {
+            List<string> tagList = new List<string>();
+
+            if (item != null)
+            {
+                
+                foreach (Item dataSourceItem in Sitecore.Context.Item.GetDataSourceItems())
+                {
+                    if(dataSourceItem.Fields["Tag"] != null)
+                    {
+                        tagList.Add(dataSourceItem.Fields["Tag"].Value.ToString());
+
+                    }
+                }
+                return tagList;
+
+            }
+            return tagList;
+     
+        }
+    }
+}
